2dd611a8e381f39770e4544a06eafb6d3c51214c
Shivamingale3 <shivamingale3@gmail.com>
2026-01-05 18:08:17 +0530

    feat: Set up NativeWind and replace default tab navigation with a root home screen.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+const HomeScreen = () => {
+  return (
+    <View className="w-full h-full flex justify-center items-center bg-white text-red-500">
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
